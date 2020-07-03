b06895222bf128e209f95dcdbf87b1e2f1c735b7
Babs95 <babsco95@gmail.com>
2020-07-03 01:12:23 +0000

    gestion login api

--- app/src/main/java/com/example/samafacture/MainActivity.java
+++ app/src/main/java/com/example/samafacture/MainActivity.java
@@ -9,6 +9,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText txtLogin, txtPassword;
@@ -34,13 +44,63 @@ public class MainActivity extends AppCompatActivity {
                     String error = getString(R.string.error_fields);
                     Toast.makeText(MainActivity.this, error, Toast.LENGTH_SHORT).show();
                 } else {
-                    //authentification(login, password);
-                    String logn = "Bbasb";
+                    authentification(login, password);
+                    /*String logn = "Bbasb";
                     Intent intent = new Intent(MainActivity.this, HomeActivity.class);
                     intent.putExtra("ExtraLogin", logn);
-                    startActivity(intent);
+                    startActivity(intent);*/
                 }
             }
         });
+    }
+
+    public void  authentification(String login, String password) {
+        try {
+            String url ="https://api-samafacture.herokuapp.com/api/user/GetLogin/"+login+"/"+password;
+            OkHttpClient  client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, final IOException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String error = getString(R.string.error_connection);
+                            Toast.makeText(MainActivity.this, error, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        JSONObject jo = new JSONObject(response.body().string());
+                        String status = jo.getString("status");
+
+                        if(status.equalsIgnoreCase("KO")) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    String message =getString(R.string.error_parameters);
+                                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        } else {
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+
+                    }catch (Exception e){
+
+                    }
+                }
+            });
+
+        } catch (Exception e){
+
+        }
     }
 }
