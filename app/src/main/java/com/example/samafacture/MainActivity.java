b0f56a1e852f73fe5daf4908ef1d1bb17fce0b00
Babs95 <babsco95@gmail.com>
2020-07-05 02:43:31 +0000

    update

--- app/src/main/java/com/example/samafacture/MainActivity.java
+++ app/src/main/java/com/example/samafacture/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.samafacture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.github.ybq.android.spinkit.style.Wave;
 
 import org.json.JSONObject;
 
@@ -24,15 +30,30 @@ public class MainActivity extends AppCompatActivity {
     private EditText txtLogin, txtPassword;
     private Button btnConnect, btnSignIn;
     private  String login, password;
+    ProgressBar progressBar;
+    Wave wave2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
         txtLogin = findViewById(R.id.txtLogin);
         txtPassword = findViewById(R.id.txtPassword);
         btnConnect = findViewById(R.id.btnConnect);
         btnSignIn = findViewById(R.id.btnSignIn);
+        //Button
+        /*wave2 = new Wave();
+        wave2.setBounds(0, 0, 100, 100);
+        wave2.setColor(getResources().getColor(R.color.colorAccent));
+        //wave2.stop();
+        btnConnect.setCompoundDrawables(wave2, null, null, null);*/
+
+        //Progessbar
+        progressBar = (ProgressBar)findViewById(R.id.spin_kit);
+        Wave wave = new Wave();
+        progressBar.setIndeterminateDrawable(wave);
+        progressBar.setVisibility(View.GONE);
 
         btnConnect.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
                     String error = getString(R.string.error_fields);
                     Toast.makeText(MainActivity.this, error, Toast.LENGTH_SHORT).show();
                 } else {
+                    //progressBar.setVisibility(View.INVISIBLE);
                     authentification(login, password);
                     /*String logn = "Bbasb";
                     Intent intent = new Intent(MainActivity.this, HomeActivity.class);
@@ -55,6 +77,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void  authentification(String login, String password) {
+        //new YourAsyncTask().execute();
+        //if (login!=null) {
+
+        //}
+
+        progressBar.setVisibility(View.VISIBLE);
+        //wave2.start();
         try {
             String url ="https://api-samafacture.herokuapp.com/api/user/GetLogin/"+login+"/"+password;
             OkHttpClient  client = new OkHttpClient();
@@ -67,6 +96,9 @@ public class MainActivity extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
+                            //if (progressBar.getVisibility() == View.VISIBLE) {
+                                progressBar.setVisibility(View.GONE);
+                            //}
                             String error = getString(R.string.error_connection);
                             Toast.makeText(MainActivity.this, error, Toast.LENGTH_SHORT).show();
                         }
@@ -83,14 +115,32 @@ public class MainActivity extends AppCompatActivity {
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
+                                    progressBar.setVisibility(View.GONE);
+                                    //wave2.stop();
                                     String message =getString(R.string.error_parameters);
                                     Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
                                 }
                             });
 
                         } else {
-                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                            startActivity(intent);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressBar.setVisibility(View.GONE);
+                                    AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                                    dialog.setIcon(R.mipmap.ic_launcher);
+                                    dialog.setTitle("Connexion réussie");
+                                    dialog.setMessage("Bienvenue dans Sama Facture!!");
+                                    dialog.setPositiveButton("Continuez", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                                            startActivity(intent);
+                                        }
+                                    });
+                                    dialog.show();
+                                }
+                            });
                         }
 
                     }catch (Exception e){
@@ -103,4 +153,5 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
 }
